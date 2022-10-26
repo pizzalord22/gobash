ba17cc675f66c6a2af32ea048256f7660013c447
pizzalord22 <markjoling3@gmail.com>
2022-10-26 18:44:24 +0200

    improved helpCommands.go, added constant for windows os

--- commandline.go
+++ commandline.go
@@ -1,48 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fatih/color"
-	"os/exec"
-	"runtime"
+    "fmt"
+    "github.com/fatih/color"
+    "os/exec"
+    "runtime"
 )
+
+const windows = "windows"
 
 // execute commands like it is a real os
 func execute(command []string) {
-	var s string
-	osName := runtime.GOOS
-	for _, v := range command {
-		s += v + " "
-	}
-	if osName == "windows" {
-		execWindows(s)
-	} else {
-		execSane(s)
-	}
+    var s string
+    osName := runtime.GOOS
+    for _, v := range command {
+        s += v + " "
+    }
+    if osName == windows {
+        execWindows(s)
+    } else {
+        execSane(s)
+    }
 }
 
 // executes commands on a windows machine
 func execWindows(s string) {
-	result, err := exec.Command("cmd", "/C", s).CombinedOutput()
-	if err != nil {
-		color.Red(fmt.Sprintf("gobash> The following error appeared when executing a command; %v\n", err))
-		return
-	}
-	color.Green("gobash> executed: %v", s)
-	if len(result) > 0 {
-		fmt.Println(string(result))
-	}
+    result, err := exec.Command("cmd", "/C", s).CombinedOutput()
+    if err != nil {
+        color.Red(fmt.Sprintf("gobash> The following error appeared when executing a command; %v\n", err))
+        return
+    }
+    color.Green("gobash> executed: %v", s)
+    if len(result) > 0 {
+        fmt.Println(string(result))
+    }
 }
 
 // executes commands the sane way
 func execSane(s string) {
-	result, err := exec.Command("/bin/sh", "-c", s).CombinedOutput()
-	if err != nil {
-		color.Red(fmt.Sprintf("gobash> The following error appeared when executing a command; %v\n", err))
-		return
-	}
-	color.Green("gobash> executed: %v", s)
-	if len(result) > 0 {
-		fmt.Println(string(result))
-	}
+    result, err := exec.Command("/bin/sh", "-c", s).CombinedOutput()
+    if err != nil {
+        color.Red(fmt.Sprintf("gobash> The following error appeared when executing a command; %v\n", err))
+        return
+    }
+    color.Green("gobash> executed: %v", s)
+    if len(result) > 0 {
+        fmt.Println(string(result))
+    }
 }
